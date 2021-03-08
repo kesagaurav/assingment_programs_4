14e2a5384aae5859019d6b2af69b871b8313a6e1
kesa gaurav <kesagaurav@gmail.com>
2021-03-08 10:49:18 +0530

    exception assignment

--- StringBufferAssingnment/src/eg1/SumOfEvenAndOdd.java
+++ StringBufferAssingnment/src/eg1/SumOfEvenAndOdd.java
@@ -8,7 +8,7 @@ public class SumOfEvenAndOdd {
 		String s="i am a very good programmer";
 		for(int i=0;i<s.length();i++) {
 			if(i%2!=0) {
-				System.out.println("at even is" + s.toUpperCase().charAt(i));
+				System.out.println("at even is" + s.toUpperCase().charAt(0));
 			}else {
 				System.out.println("at odd is" + s.toLowerCase().charAt(i));
 			}
